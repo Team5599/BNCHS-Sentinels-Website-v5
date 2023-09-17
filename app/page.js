35b8a1afc90f75d64012843b4f4c70a04af8a010
Michael Rooplall <MichaelRooplall@gmail.com>
2023-09-17 05:04:13 -0400

    Add Home Tiles

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 import Navbar from '@components/Navbar/Navbar'
 import Header from '@components/Header/Header'
 import SubheaderShape from '@components/SubheaderShape/SubheaderShape'
+import HomeTiles from './components/HomeTiles/HomeTiles'
 
 export default function Home() {
 	return (
@@ -34,9 +35,10 @@ export default function Home() {
 					The Sentinels are Benjamin N. Cardozo High School's Robotics Team. We compete in various annual robotics competitions against high schools across the globe, raising awareness for Science, Technology, Engineering, and Mathematics (STEM), along with teaching students aspects behind business and marketing, logistics, and media. We also participate in various community and school events.
 				</p>
 				<p>
-					The Sentinels are Benjamin N. Cardozo High School's Robotics Team. We compete in various annual robotics competitions against high schools across the globe, raising awareness for Science, Technology, Engineering, and Mathematics (STEM), along with teaching students aspects behind business and marketing, logistics, and media. We also participate in various community and school events.
+					FRC Team 5599, participates in the annual FIRST® Robotics Competition (FRC). In a six week time span, we raise funds, design, and construct a fully-functional robot.
 				</p>
 			</div>
+			<HomeTiles/>
 		</div>
 	)
 }
