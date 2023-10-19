45517e6c976b0f7997e071186eb74688c80fb82c
Michael Rooplall <MichaelRooplall@gmail.com>
2023-10-18 21:09:57 -0400

    Team Person Cards

--- app/Team/page.js
+++ app/Team/page.js
@@ -1,4 +1,7 @@
+'use client'
 import styles from './page.module.css'
+
+import {useState, useEffect} from 'react'
 
 import Navbar from '@components/Navbar/Navbar'
 import Header from '@components/Header/Header'
@@ -8,31 +11,81 @@ import PersonCard from '@components/PersonCard/PersonCard'
 
 import FilterHeader from '@components/FilterHeader/FilterHeader'
 
-const LeadershipContainer = () => {
+const PersonCardContainer = ({title, size, members, contrast}) => {
+	return (
+		<div className={`container ${styles.personBody}`}>
+			<h3 style={{
+				color : contrast ? '#fff' : '#000'
+			}}>
+				{title}
+			</h3>
+			<div className={styles.personBodyContents}>
+				{
+					members.map((personItem) => {
+						return <PersonCard key={personItem._id} personData={personItem}/>
+					})
+				}
+			</div>
+		</div>
+	)
+}
+
+const LeadershipContainer = ({members}) => {
 	return (
 		<div className={styles.leadershipSection}>
 			<svg className={styles.triangleSVG} width="100%" height="120px" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 120L0 120 307.75 0 1200 120z" className="shape-fill" fill="#F68313" fillOpacity="1"></path></svg>
 			<div className={styles.leadershipBlock}>
-
-				<div className={`container ${styles.leadershipBody}`}>
-					<h3>
-						LEADERSHIP
-					</h3>
-					<div className={styles.leadershipBodyContents}>
-						{
-							[1, 2, 3, 4, 5].map((personItem) => {
-								return <PersonCard key={personItem}/>
-							})
-						}
-					</div>
-				</div>
+				<PersonCardContainer title={'LEADERSHIP'} size='lg' members={members} contrast={true}/>				
 			</div>
 			<svg className={styles.triangleSVG} width="100%" height="120px" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 0L0 0 892.25 120 1200 0z" className="shape-fill" fill="#F68313" fillOpacity="1"></path></svg>
 		</div>
 	)
 }
 
+const getTeamData = async (season) => {
+    try {
+        const res = await fetch(
+            `https://beta.team5599.com/api/v1/Team/${season || 'current'}`,
+            {
+                method: 'GET'
+            }
+        );
+
+        const teamData = await res.json();
+
+        console.log(teamData);
+
+        return teamData.payload;
+
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+
 export default function Team() {
+
+	const [teamData, setTeamData] = useState(
+		{
+			leadership : [],
+			mentors : [],
+			roster : [],
+			media : {url : '', caption : ''}
+		}
+	);
+
+    useEffect(() => {
+
+        if (typeof window === 'undefined') return;
+
+        const fetchData = async () => {
+            setTeamData(await getTeamData(2020));
+        }
+
+        fetchData();
+
+    }, [])
+
 	return (
 		<div>
 			<Navbar/>
@@ -46,7 +99,9 @@ export default function Team() {
 			<div className='container'>
 				<FilterHeader/>
 			</div>
-			<LeadershipContainer/>
+			<LeadershipContainer members={teamData.leadership}/>
+			<PersonCardContainer title={'MEMBERS'} size='md' members={teamData.roster}/>	
+			<PersonCardContainer title={'MENTORS'} size='md' members={teamData.mentors}/>	
 			<Footer/>
 		</div>
 	)
--- app/components/PersonCard/PersonCard.jsx
+++ app/components/PersonCard/PersonCard.jsx
@@ -2,42 +2,22 @@ import styles from './personCard.module.css'
 
 import Image from 'next/image';
 
-const PersonCard = () => {
+const PersonCard = ({personData}) => {
     return (
         <div className={styles.personCard}>
-            <div style={{
-                position : 'absolute',
-                top : 0,
-                left : 0,
-                width : 140,
-                height : 140,
-                backgroundColor : '#ffffff40',
-                transform: 'rotate(25deg)'
-            }}/>
-            <div style={{
-                position : 'absolute',
-                top : 46,
-                left : 20,
-                width : 140,
-                height : 140,
-                backgroundColor : '#4040ff40',
-                transform: 'rotate(25deg)'
-            }}/>
-            <div className={styles.personCardImageContainer}>
-                <Image
-                    unoptimized
-                    src={'https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/30836/square_thumb%403x.jpg'}
-                    alt={'Placeholder'}
-                    width={160}
-                    height={160}
-                    style={{
-                        borderRadius : 160,
-                        width : 160,
-                        height: 160,
-                        borderWidth : 2,
-                        borderColor : '#fff'
-                    }}
-                />
+            <Image
+                src={personData.image}
+                className={styles.personCardImage}
+                fill={true}
+                unoptimized
+                alt={`${personData.name} | Roles:`}
+            />
+            <div className={styles.cardOverlay}>
+               <span>{personData.name}</span>
+               <span>DIRECTOR OF TITLING</span>
+               <div>
+                    <span>Social - Media - Icons</span>
+               </div>
             </div>
         </div>
     )
