78e6104dd7a8a28ed9bb23b43a0e7ded34934747
Michael Rooplall <MichaelRooplall@gmail.com>
2023-11-07 17:24:05 -0500

    Finish Sponsors grid

--- app/components/Sponsors/Sponsors.jsx
+++ app/components/Sponsors/Sponsors.jsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useRef, useCallback } from 'react';
 
 import Link from 'next/link';
 import NextImage from 'next/image';
 import styles from './sponsors.module.css';
 
 import useWindowDimensions from '@lib/useWindowDimensions';
+import { useContainerDimensions } from '@/lib/useContainerDimensions';
 
 const getSponsorsData = async () => {
 
@@ -83,18 +84,10 @@ const Sponsors = () => {
             // Get SponsorsData
             let sponsorsData = await getSponsorsData();
 
-			console.log("1", sponsorsData);
-
-
-            console.log("Preloading image metadata");
             // Preload images and get their dimensions
             const sponsorImageMetadata = await Promise.allSettled( 
                 sponsorsData.map((sponsorData) => getImageDimensionsFromURL(sponsorData.id, sponsorData.srcURL))
             )
-
-			console.log("2", sponsorImageMetadata);
-
-            console.log("Getting column weights")
 			
             // Get their 'column weight' to determine their weight (1, 2, 3)
             sponsorImageMetadata.forEach((imageMetadataPromise) => {
@@ -122,7 +115,6 @@ const Sponsors = () => {
 				return (sponsorData.hasOwnProperty('metadata'));
 			})
 
-            console.log("3", sponsorsData);
             setSponsorsData(sponsorsData);
 
         })();
@@ -130,8 +122,10 @@ const Sponsors = () => {
     }, [])
 
     function calculateNumGridColumns(screenWidth) {
-        if (screenWidth < 992) return 3;
-        return 5;
+        if (screenWidth < 768) return 2;
+        if (screenWidth < 992) return 4;
+        if (screenWidth < 1222) return 5;
+        return 6;
     }
 
     const { _, width } = useWindowDimensions();
@@ -140,84 +134,276 @@ const Sponsors = () => {
         return calculateNumGridColumns(width)
     },  [width])
 
-    const packSponsorItems = (numGridColumns, sponsorsData) => {
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    }
+
+
+    const packSponsorItems = useCallback((numGridColumns, sponsorsData, disableBacktracking, shuffleInFillers) => {
 
         if (sponsorsData.length == 0) return [];
 
-        const maxWeight = numGridColumns;
+        let items = [...sponsorsData].reverse();
+        let fitItemsNo = 0;
 
-        const result = [];
+        const rows = [];
+        let currentRow = [];
+        let currentRowWeight = 0;
 
-        let currentBin = [];
-        let currentWeight = 0;
+        let lastFitAttemptTracker = {};
 
-        for (const item of sponsorsData) {
-            if (currentWeight + item.metadata.columnWeight <= maxWeight) {
-                currentBin.push(item);
-                currentWeight += item.metadata.columnWeight;
-            } else {
-                while (currentWeight < maxWeight) {
-                    for (const candidate of sponsorsData) {
-                        if (currentWeight + candidate.metadata.columnWeight <= maxWeight) {
-                            currentBin.push(candidate);
-                            currentWeight += candidate.metadata.columnWeight;
+        if (shuffleInFillers) {
+
+            function seededRandomNumber(seed) {
+                const x = Math.sin(seed++) * 10000;
+                return x - Math.floor(x);
+            }
+        
+            function shuffle(array, seed) {
+                let currentIndex = array.length,  randomIndex;
+              
+                // While there remain elements to shuffle.
+                while (currentIndex > 0) {
+              
+                    // Pick a remaining element.
+                    randomIndex = Math.floor(seededRandomNumber(seed) * currentIndex);
+                    currentIndex--;
+                
+                    // And swap it with the current element.
+                    [array[currentIndex], array[randomIndex]] = [
+                        array[randomIndex], array[currentIndex]
+                    ];
+                    ++seed;
+                }
+              
+                return array;
+            }
+
+            const sponsorsDataTotalWeight = sponsorsData.reduce((acc, curr) => {
+                return acc + curr.metadata.columnWeight;
+            }, 0)
+
+            let numFillers = numGridColumns - (sponsorsDataTotalWeight % numGridColumns);
+
+            if (numFillers == numGridColumns) {
+                numFillers = 0;
+            }
+
+            console.log(`Requires ${numFillers} fillers to fill grid`);
+
+            items.push(...Array(numFillers).fill({
+                id : 'filler',
+                metadata : {columnWeight : 1},
+                type : 'prefill'
+            }))
+
+            for (let i = 0; i < 3; i++){
+                items = shuffle(items, 10);
+            }
+            
+
+        }
+
+        
+
+        /*
+            Iterate through each sponsor item.
+            If an item can be placed in the current row, place it and remove it from the available pool
+            If it cannot be placed in the row, try the next item until exhausted
+            If no items fit in the row, create a new row and repeat. Fill the abandoned row with 1x1 fillers
+
+            If disableBacktracking is true, we only do one pass over all the items. As soon as an item can't fit in a row, we fill it with spacers, create a new row, and proceed iterating over the items.
+        */
+
+        let iteration = 0;
+
+        console.log("Fitting", [...items], "in columns:", numGridColumns);
+
+        iteration_loop : while (items.length > 0){
+
+            console.log("Iteration", iteration, items.length);
+            // Attempt to fill current row with all sponsor items
+            for (let i = items.length - 1; i >= 0; i--){
+
+                const sponsorItem = items[i];
+
+                if (sponsorItem.metadata.columnWeight >= numGridColumns){
+                    // Unique case where the item is as wide as or wider than the available columns, so we force it into it's own row
+                    rows.push([sponsorItem]);
+                    fitItemsNo++;
+                    items.splice(i, 1);
+
+                    console.log("Whole row fit for item", sponsorItem);
+                    continue;
+                }
+
+                if (currentRowWeight + sponsorItem.metadata.columnWeight <= numGridColumns) {
+                    // Add to current row
+                    currentRow.push(sponsorItem);
+                    currentRowWeight = currentRowWeight + sponsorItem.metadata.columnWeight;
+                    fitItemsNo++;
+                    // Remove from list of available items
+                    items.splice(i, 1);
+
+                    console.log("Good fit for item", sponsorItem);
+                    
+                    // If we are out of items
+                    // If there was backtracking, fill remaining space with fillers and send the current row off
+                    // If there was NO backtracking, do not fill the space with fillers, attempt to fill with remaining items . . .
+                    if (items.length == 0){
+
+
+                        if (disableBacktracking == true){
+                            let remainingWeight = numGridColumns - currentRowWeight;
+                        
+                            currentRow.push(...Array(remainingWeight).fill({
+                                id : 'filler',
+                                metadata : {columnWeight : 1}
+                            }));
                         }
+                        
+                        rows.push(currentRow);
+
+                        continue;
+                    }
+
+                    // If the row is full, send it off and create a new row to fill
+                    if (currentRowWeight == numGridColumns) {
+                        rows.push(currentRow);
+                        currentRow = [];
+                        currentRowWeight = 0;
+                        console.log("New row (from filled)");
+                    }
+
+                    continue;
+                }
+
+                // If the item doesn't fit, and backtracking is disabled (or we're in a loop and can't fit an item), fill the current row with fillers, send it off, and create a new row with the item that didn't fit.
+
+                if (disableBacktracking || (lastFitAttemptTracker[sponsorItem.id] && lastFitAttemptTracker[sponsorItem.id] > 1)) {
+
+                    // Fill the current row with fillers and send it off
+                    let remainingWeight = numGridColumns - currentRowWeight;
+                
+                    currentRow.push(...Array(remainingWeight).fill({
+                        id : 'filler',
+                        metadata : {columnWeight : 1}
+                    }));
+                    rows.push(currentRow);
+
+                    // Create a new row with the item that didn't fit
+                    currentRow = [sponsorItem];
+                    currentRowWeight = sponsorItem.metadata.columnWeight;
+                    fitItemsNo++;
+
+                    // Remove from list of available items
+                    items.splice(i, 1);
+                    
+                    console.log("New row (from disableBacktracking)");
+
+
+                } else {
+                    console.log("Skipping fit for item", sponsorItem);
+
+                    if (iteration > 100){
+                        console.log("Fit Overflow");
+                        break iteration_loop;
+                    }
+
+                    if (lastFitAttemptTracker[sponsorItem.id]) {
+                        lastFitAttemptTracker[sponsorItem.id]++
+                    } else {
+                        lastFitAttemptTracker[sponsorItem] = 1;
                     }
                 }
-                result.push(currentBin);
-                currentBin = [item];
-                currentWeight = item.metadata.columnWeight;
+
+                
             }
-        }
 
-        if (currentBin.length > 0) {
-            while (currentWeight < maxWeight) {
-                for (const candidate of sponsorsData) {
-                    if (currentWeight + candidate.metadata.columnWeight <= maxWeight) {
-                        currentBin.push(candidate);
-                        currentWeight += candidate.metadata.columnWeight;
-                    }
-                }
+            if (items.length == 0) break;
+
+            // There are items left that did not fit in the current row
+            // Fill any remaining space in the row if it's a non-empty row
+            if (currentRowWeight !== 0 && disableBacktracking == true && shuffleInFillers == true){
+
+                let remainingWeight = numGridColumns - currentRowWeight;
+            
+                currentRow.push(...Array(remainingWeight).fill({
+                    id : 'filler',
+                    metadata : {columnWeight : 1}
+                }));
+                rows.push(currentRow);
+
+                // Create a new row and try to fill the remaining items
+                currentRow = [];
+                currentRowWeight = 0;
+
             }
-            result.push(currentBin);
+
+
+            iteration++;
         }
+        
 
-		console.log("PACKED", numGridColumns, sponsorsData.length, result);
-        return result;
+        console.log("PACKED", numGridColumns, sponsorsData.length, fitItemsNo, rows);
+        return rows;
+    }, [])
 
-    }
 
-    const packedSponsorItems = useMemo(() => packSponsorItems(numGridColumns, sponsorsData), [numGridColumns, sponsorsData])
+    const packedSponsorItems = useMemo(() => packSponsorItems(numGridColumns, sponsorsData, false, true), [numGridColumns, packSponsorItems, sponsorsData])
+
+    const containerRef = useRef();
+    const { width : containerWidth} = useContainerDimensions(containerRef);
+    const gridGap = 20;
+    const itemHeight = useMemo(() => {
+        return (containerWidth - (gridGap * (numGridColumns - 1)))/numGridColumns;
+    }, [containerWidth, numGridColumns])
+    
+    
+
+    // item height = [(width of container) - (gap * n - 1)]/(n)
 
     return (
 		<>
 			<div
+                ref={containerRef}
 				style={{
-					display : 'flex',
-					justifyContent : 'center',
-					flexDirection : 'column',
-					gap : 20
+                    display : 'grid',
+                    gridTemplateColumns: `repeat(${numGridColumns}, 1fr)`,
+                    gap : gridGap
 				}}
 			>
 				{
-					packedSponsorItems.map((row, index) => {
-						return <div
-							key={index}
-							style={{
-								display : 'flex',
-								flexDirection : 'row',
-							}}
-						>
+					packedSponsorItems.map((row, row_index) => {
+						return <>
 							{
-								row.map((sponsorItem) => {
+								row.map((sponsorItem, column_index) => {
+                                    if (sponsorItem.id == "filler") {
+                                        const randomBackgroundColor = (() => {
+                                            const num = getRandomInt(0, 1);
+                                            if (num == 0) return '#027bffa0';
+                                            if (num == 1) return '#f68313a0';
+                                        })();
+                                        return <div
+                                            key={`filler-${row_index}-${column_index}`}
+                                            style={{
+                                                backgroundColor: randomBackgroundColor,
+                                                gridColumn : `span 1`,
+                                                height : itemHeight,
+                                                gridRow : `${row_index} / span 1`,
+                                            }}
+                                        />
+                                    }
 									return <div
 										key={sponsorItem.id}
 										style={{
 											display : 'flex',
-											width : sponsorItem.metadata.columnWeight * 120,
-											aspectRatio : sponsorItem.metadata.columnWeight/1,
-											backgroundColor : '#eee',
-											position : 'relative',
+                                            gridColumn : `span ${sponsorItem.metadata.columnWeight > numGridColumns ? numGridColumns : sponsorItem.metadata.columnWeight}`,
+                                            height : itemHeight,
+                                            gridRow : `${row_index} / span 1`,
+                                            position : 'relative',
 										}}
 									>
 										<NextImage
@@ -233,7 +419,7 @@ const Sponsors = () => {
 									</div>
 								})
 							}
-						</div>
+						</>
 					})
 				}
 			</div>
