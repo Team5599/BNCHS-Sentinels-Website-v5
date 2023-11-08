a4bc0783f89264a59267a521b10a8c255fb68b28
Michael Rooplall <MichaelRooplall@gmail.com>
2023-11-08 11:51:47 -0500

    Person Card design updates

--- app/components/PersonCard/PersonCard.jsx
+++ app/components/PersonCard/PersonCard.jsx
@@ -5,20 +5,58 @@ import Image from 'next/image';
 const PersonCard = ({personData}) => {
     return (
         <div className={styles.personCard}>
-            <Image
-                src={personData.image}
-                className={styles.personCardImage}
-                fill={true}
-                unoptimized
-                alt={`${personData.name} | Roles:`}
-            />
-            <div className={styles.cardOverlay}>
-               <span>{personData.name}</span>
-               <span>DIRECTOR OF TITLING</span>
-               <div>
-                    <span>Social - Media - Icons</span>
-               </div>
+            <div
+                className={styles.personCardImageContainer}
+            >
+                <Image
+                    src={personData.image}
+                    className={styles.personCardImage}
+                    fill={true}
+                    unoptimized
+                    alt={`${personData.name} [MAIN ROLE]:`}
+                />
             </div>
+            <div className={styles.personCardBottomsheet}>
+                <div
+                    style={{
+                        display : 'flex',
+                        flexDirection : 'column'
+                    }}
+                >
+                    <span
+                        style={{
+                            fontWeight : 600,
+                            fontSize: 22
+                        }}
+                    >
+                        {personData.name}
+                    </span>
+                    <span
+                        style={{
+                            fontWeight : 500,
+                            fontSize : 14
+                        }}
+                    >
+                        DIRECTOR OF TITLING
+                    </span>
+                    <span
+                        className={styles.quoteBlock}
+                        style={{
+                            marginTop : 10,
+                            color : '#ccc'
+                        }}
+                    >
+                        Quote Block
+                    </span>
+                </div>
+                
+                <div style={{
+                    textAlign : 'center'
+                }}>
+                    <span>Social - Media - Icons</span>
+                </div>
+            </div>
+            <svg style={{maxWidth : '100%', height : 40}} width="100vw" height="40px" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 40" preserveAspectRatio="none" ><path  d="M300 0L0 0 300 40 300 0z" className="shape-fill" fill="#1b1616" fillOpacity="1"></path></svg>
         </div>
     )
 }
