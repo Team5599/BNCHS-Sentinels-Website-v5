1140540f7b881d4fd7d210e23c76ead3e51ec033
Michael Rooplall <MichaelRooplall@gmail.com>
2023-11-07 19:16:32 -0500

    Add History Page base

--- app/History/page.js
+++ app/History/page.js
@@ -2,30 +2,32 @@ import styles from './page.module.css'
 
 import Navbar from '@components/Navbar/Navbar'
 import Header from '@components/Header/Header'
+import Footer from '@components/Footer/Footer'
 import SubheaderShape from '@components/SubheaderShape/SubheaderShape'
 
 export default function History() {
 	return (
 		<div>
 			<Navbar/>
-			<Header size='lg' imageClass={styles.headerOne} gradient={true}>
-				<div className='container restrictHeader' style={{color : '#fff'}}>
-					<h1 style={{fontWeight : 900, marginBottom : 20}}>
-						WE ARE THE SENTINELS
-					</h1>
+			<div
+				className={styles.gradientBackground}
+				style={{
+					paddingTop : 40,
+					minHeight : 400,
+					paddingBottom : 120,
+					marginBottom : -60
+				}}
+			>
+				<div className='container' style={{display: 'flex', flexDirection : 'column', gap : 20, textAlign : 'justify', color : '#fff'}}>
 					<h2>
-						FIRST (C) Robotics Competition Team 5599, from Benjamin N. Cardozo High School in Bayside, New York
+						OUR HISTORY
 					</h2>
-				</div>
-			</Header>
-			<SubheaderShape>
-				<div className='container'>
-					<p className='subheading' style={{color : '#fff'}}>
-						{ /* eslint-disable-next-line react/no-unescaped-entities */ }
-						The Sentinels are Benjamin N. Cardozo High School's Robotics Team. We compete in various annual robotics competitions against high schools across the globe, raising awareness for Science, Technology, Engineering, and Mathematics (STEM), along with teaching students aspects behind business and marketing, logistics, and media. We also participate in various community and school events.
+					<p>
+						Lorem ipsum blah blah blah
 					</p>
 				</div>
-			</SubheaderShape>
+			</div>
+			<Footer/>
 		</div>
 	)
 }
