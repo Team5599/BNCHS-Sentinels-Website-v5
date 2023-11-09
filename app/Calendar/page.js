8907f80cfee8d484bf8c09b108af4ca73be9c16e
Michael Rooplall <MichaelRooplall@gmail.com>
2023-11-09 16:26:03 -0500

    Use proxy loader to bypass Calendar errors

--- app/Calendar/page.js
+++ app/Calendar/page.js
@@ -2,6 +2,8 @@
 
 import styles from './page.module.css'
 
+import { useState, useEffect } from 'react'
+
 import Navbar from '@components/Navbar/Navbar'
 import Footer from '@components/Footer/Footer'
 
@@ -15,11 +17,45 @@ const calendars = [
 ]
 
 export default function Calendar() {
+
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+		setLoaded(true);
+	}, [])
+	
+
 	return (
 		<div>
 			<Navbar/>
-			<div className='container' style={{display: 'flex', flexDirection : 'column', gap : 20, paddingTop : 40, paddingBottom : 40, textAlign : 'justify'}}>
-				<GoogleCalendar calendars={calendars}/>
+			<div className='container' style={{display: 'flex', flexDirection : 'column', gap : 20, paddingTop : 120, paddingBottom : 120, maxWidth : 1200, justifyContent : 'center',  textAlign : 'justify'}}>
+				{
+					(loaded) ?
+					<GoogleCalendar calendars={calendars}/>
+					: <div
+						style={{
+							backgroundColor : '#eee',
+							width : '100%',
+							maxWidth : 1200,
+							height : '50vh',
+							display: 'flex',
+							justifyContent : 'center',
+							alignItems : 'center'
+						}}
+					>
+						<span
+							style={{
+								fontSize : 22,
+								fontWeight : 600,
+								backgroundColor : '#000',
+								padding : '20px 80px',
+								color : '#fff'
+							}}
+						>
+							Loading Calendar
+						</span>
+					</div>
+				}
 			</div>
 			<Footer/>
 		</div>
