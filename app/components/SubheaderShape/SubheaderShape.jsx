0dd7a9dd9809808d245526f08a6900f6072c2bf3
Michael Rooplall <MichaelRooplall@gmail.com>
2023-11-09 16:00:24 -0500

    Add 'small' size to SubheaderShape

--- app/components/SubheaderShape/SubheaderShape.jsx
+++ app/components/SubheaderShape/SubheaderShape.jsx
@@ -1,13 +1,19 @@
 import styles from './subheaderShape.module.css';
 import Image from 'next/image';
 
-const SubheaderShape = ({children}) => {
+const SubheaderShape = ({children, size}) => {
     return (
         <div className={styles.subheaderShape}>
-            <div className={styles.subheaderShapeBody}>
+            <div
+                className={styles.subheaderShapeBody}
+                style={{
+                    paddingTop : size == 'sm' ? 40 : 80,
+                    paddingBottom : size == 'sm' ? 40 : 80
+                }}
+            >
                 {children}
             </div>
-            <svg className={styles.triangleSVG} style={{maxWidth : '100%'}} width="100vw" height="120px" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 0L0 0 892.25 114.72 1200 0z" className="shape-fill" fill="#F68313" fillOpacity="1"></path></svg>
+            <svg style={{maxWidth : '100%', height : size == 'sm' ? 60 : 120}} width="100vw" height={size == 'sm' ? "60px" : "120px"} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 0L0 0 892.25 114.72 1200 0z" className="shape-fill" fill="#F68313" fillOpacity="1"></path></svg>
         </div>
     )
 }
