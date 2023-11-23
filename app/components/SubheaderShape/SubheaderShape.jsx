c9d146624a5e8d0f8d68f9afb6a7eb51166db63c
Michael Rooplall <MichaelRooplall@gmail.com>
2023-11-23 03:06:03 -0500

    Fixes for Mobile Views

--- app/components/SubheaderShape/SubheaderShape.jsx
+++ app/components/SubheaderShape/SubheaderShape.jsx
@@ -3,17 +3,13 @@ import Image from 'next/image';
 
 const SubheaderShape = ({children, size}) => {
     return (
-        <div className={styles.subheaderShape}>
+        <div className={`${styles.subheaderShape} ${size == 'sm' && styles.small}`}>
             <div
                 className={styles.subheaderShapeBody}
-                style={{
-                    paddingTop : size == 'sm' ? 40 : 80,
-                    paddingBottom : size == 'sm' ? 40 : 80
-                }}
             >
                 {children}
             </div>
-            <svg style={{maxWidth : '100%', height : size == 'sm' ? 60 : 120}} width="100vw" height={size == 'sm' ? "60px" : "120px"} data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 0L0 0 892.25 114.72 1200 0z" className="shape-fill" fill="#F68313" fillOpacity="1"></path></svg>
+            <svg className={styles.triangleSVG} width="100vw" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" ><path  d="M1200 0L0 0 892.25 114.72 1200 0z" className="shape-fill" fill="#F68313" fillOpacity="1"></path></svg>
         </div>
     )
 }
