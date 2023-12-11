a585b43aaa11ee4dbbd101658211f31279439772
Michael Rooplall <MichaelRooplall@gmail.com>
2023-12-11 09:29:19 -0500

    Change SVG props from html to react

--- app/Contact/page.js
+++ app/Contact/page.js
@@ -16,7 +16,7 @@ import { Button } from '@components/Button/Button'
 
 const PhoneIconSVG = () => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+		<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 			<path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
 		</svg>
@@ -25,7 +25,7 @@ const PhoneIconSVG = () => {
 
 const InstagramIconSVG = () => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+		<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
 			<rect x="4" y="4" width="16" height="16" rx="4" />
 			<circle cx="12" cy="12" r="3" />
@@ -36,7 +36,7 @@ const InstagramIconSVG = () => {
 
 const EmailIconSVG = () => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+		<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 			<path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
 			<path d="M3 7l9 6l9 -6"></path>
@@ -46,7 +46,7 @@ const EmailIconSVG = () => {
 
 const PinIconSVG = () => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+		<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
 			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
 			<path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
 			<path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
