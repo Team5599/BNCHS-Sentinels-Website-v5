ab6ebf039220a2fee958041188cefdb873c6d7e2
Michael Rooplall <MichaelRooplall@gmail.com>
2023-12-11 21:06:15 -0500

    Add Coming Soon Banner

---
+++ app/components/ComingSoonBanner/ComingSoonBanner.jsx
@@ -0,0 +1,24 @@
+
+const ComingSoonBanner = () => {
+    return (
+        <div
+            style={{
+                maxWidth : '100%',
+                width : '100vw',
+                height : 80,
+                backgroundColor : '#000000a0'
+            }}
+        >
+            <span
+                style={{
+                    color : '#fff',
+                    fontSize : 32
+                }}
+            >
+                COMING SOON
+            </span>
+        </div>
+    )
+}
+
+export default ComingSoonBanner;
