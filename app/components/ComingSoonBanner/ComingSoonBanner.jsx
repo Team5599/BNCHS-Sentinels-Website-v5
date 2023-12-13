c7063159bc7e846183d3bd7d5f788ed3b24b4d2f
Michael Rooplall <MichaelRooplall@gmail.com>
2023-12-13 12:24:44 -0500

    Modify Coming Soon Banner

--- app/components/ComingSoonBanner/ComingSoonBanner.jsx
+++ app/components/ComingSoonBanner/ComingSoonBanner.jsx
@@ -5,18 +5,44 @@ const ComingSoonBanner = () => {
             style={{
                 maxWidth : '100%',
                 width : '100vw',
-                height : 80,
-                backgroundColor : '#000000a0'
+                height : 320,
+                backgroundColor : '#000000a0',
+                display : 'flex',
+                justifyContent : 'center',
+                alignItems : 'center',
+                textAlign : 'center',
+                marginTop : 40,
+                marginBottom : 80
             }}
         >
-            <span
+            <div
                 style={{
-                    color : '#fff',
-                    fontSize : 32
+                    display : 'flex',
+                    flexDirection : 'column',
+                    gap : 0
                 }}
             >
-                COMING SOON
-            </span>
+                <span
+                    style={{
+                        color : '#fff',
+                        fontSize : 46,
+                        fontWeight : 700,
+                        display : 'block'
+                    }}
+                >
+                    COMING SOON
+                </span>
+                <span
+                    style={{
+                        color : '#fff',
+                        fontSize : 24,
+                        fontWeight : 200
+                    }}
+                >
+                    No content here - coming soon!
+                </span>
+            </div>
+            
         </div>
     )
 }
