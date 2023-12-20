663be3224e35bb650b4ecdfcdf35fb90db18e62e
Michael Rooplall <MichaelRooplall@gmail.com>
2023-12-20 01:56:28 -0500

    Add Back, Previous, and Next Robots buttons to individual Robot pages

--- app/Robots/[robot]/page.js
+++ app/Robots/[robot]/page.js
@@ -72,11 +72,19 @@ export default function Robot({params}) {
 		</span>
 	}
 
-	const robotItem = robotData.find((robotItem) => {
-		return robotItem.name.replace(/ /g, "") == params.robot
-	})
+	const {robotItem, previousRobotItem, nextRobotItem} = (() => {
+		const robotItemIndex = robotData.findIndex((robotItem) => {
+			return robotItem.name.toLowerCase().replace(/ /g, "") == params.robot.toLowerCase();
+		})
+		return {
+			robotItem : robotData[robotItemIndex],
+			previousRobotItem : (robotItemIndex > 0) ? robotData[robotItemIndex - 1] : null,
+			nextRobotItem : (robotItemIndex < robotData.length - 1) ? robotData[robotItemIndex + 1] : null
+		}
+	})()
 
-	console.log("robot", robotItem, robotItem.media)
+
+	console.log("robot", robotItem, previousRobotItem, nextRobotItem, robotItem.media)
 
 	return (
 		<div>
@@ -89,7 +97,7 @@ export default function Robot({params}) {
 				>
 					<ButtonLink
 						className={styles.button}
-						href={'/robots/x'}
+						href={'/Robots'}
 						style={{
 							backgroundColor : '#000'
 						}}
@@ -98,10 +106,19 @@ export default function Robot({params}) {
 							style={{
 								paddingLeft : 20,
 								paddingRight : 20,
-								whiteSpace: 'nowrap'
+								verticalAlign : 'bottom',
+								whiteSpace : 'nowrap'
 							}}
 						>
-							Back to Robots
+							<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-back-up-double" style={{marginRight : 10, verticalAlign : 'bottom'}} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+								<path d="M13 14l-4 -4l4 -4" />
+								<path d="M8 14l-4 -4l4 -4" />
+								<path d="M9 10h7a4 4 0 1 1 0 8h-1" />
+							</svg>
+							<span>
+								Back to Robots
+							</span>
 						</span>
 					</ButtonLink>
 				</div>
@@ -196,36 +213,52 @@ export default function Robot({params}) {
 				>
 					<ButtonLink
 						className={styles.button}
-						href={'/robots/x'}
+						href={previousRobotItem ? `/Robots/${previousRobotItem.name.replace(/ /g, "")}` : '/Robots'}
 						style={{
 							backgroundColor : '#000'
 						}}
+						disabled={previousRobotItem == null}
 					>
 						<span
 							style={{
 								paddingLeft : 20,
 								paddingRight : 20,
-								whiteSpace: 'nowrap'
+								verticalAlign : 'bottom',
+								whiteSpace : 'nowrap'
 							}}
 						>
-							{'<--'} Previous Robot Name
+							<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-arrow-left-filled" style={{marginRight : 8, verticalAlign : 'bottom'}} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+								<path d="M12 2a10 10 0 0 1 .324 19.995l-.324 .005l-.324 -.005a10 10 0 0 1 .324 -19.995zm.707 5.293a1 1 0 0 0 -1.414 0l-4 4a1.048 1.048 0 0 0 -.083 .094l-.064 .092l-.052 .098l-.044 .11l-.03 .112l-.017 .126l-.003 .075l.004 .09l.007 .058l.025 .118l.035 .105l.054 .113l.043 .07l.071 .095l.054 .058l4 4l.094 .083a1 1 0 0 0 1.32 -1.497l-2.292 -2.293h5.585l.117 -.007a1 1 0 0 0 -.117 -1.993h-5.586l2.293 -2.293l.083 -.094a1 1 0 0 0 -.083 -1.32z" stroke-width="0" fill="currentColor" />
+							</svg>
+							<span>
+								Previous Robot
+							</span>
 						</span>
 					</ButtonLink>
 					<ButtonLink
 						className={styles.button}
-						href={'/robots/x'}
+						href={nextRobotItem ? `/Robots/${nextRobotItem.name.replace(/ /g, "")}` : '/Robots'}
 						style={{
 							backgroundColor : '#000'
 						}}
+						disabled={nextRobotItem == null}
 					>
 						<span
 							style={{
 								paddingLeft : 20,
 								paddingRight : 20,
-								whiteSpace: 'nowrap'
+								verticalAlign : 'bottom',
+								whiteSpace : 'nowrap',
 							}}
 						>
-							Next Robot Name {'-->'}
+							<span>
+								Next Robot
+							</span>
+							<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-arrow-right-filled" style={{marginLeft : 8, verticalAlign : 'bottom'}} width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+								<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+								<path d="M12 2l.324 .005a10 10 0 1 1 -.648 0l.324 -.005zm.613 5.21a1 1 0 0 0 -1.32 1.497l2.291 2.293h-5.584l-.117 .007a1 1 0 0 0 .117 1.993h5.584l-2.291 2.293l-.083 .094a1 1 0 0 0 1.497 1.32l4 -4l.073 -.082l.064 -.089l.062 -.113l.044 -.11l.03 -.112l.017 -.126l.003 -.075l-.007 -.118l-.029 -.148l-.035 -.105l-.054 -.113l-.071 -.111a1.008 1.008 0 0 0 -.097 -.112l-4 -4z" stroke-width="0" fill="currentColor" />
+							</svg>
 						</span>
 					</ButtonLink>
 				</div>
